5101505d343cd040c15cdfdf21be5969b0fa87c6
cz <>
2025-01-04 19:49:44 +0800

    自定义返回

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -32,70 +32,124 @@ class MainScreen extends GetView<MainScreenController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: BaseColors.white,
-        body: SafeArea(
-          child: InAppWebView(
-              initialUrlRequest: URLRequest(
-                  url: WebUri("https://prod.d2y6t2vezku3nd.amplifyapp.com/")),
-              initialSettings: InAppWebViewSettings(
-                allowsInlineMediaPlayback: true, // 允许内嵌视频播放
-                mediaPlaybackRequiresUserGesture: false, // 自动播放视频不需要用户手势
-              ),
-              onWebViewCreated: (webController) {
-                controller.webViewController = webController;
-                // 注册 JavaScript Handler
-                webController.addJavaScriptHandler(
-                  handlerName: 'onMessageReceived',
-                  callback: (args) async {
-                    if (args.isNotEmpty) {
-                      Map<String, dynamic> parsedMessage =
-                          jsonDecode(args.first);
-                      WebMessageReceivedInfo info =
-                          WebMessageReceivedInfo.fromJson(parsedMessage);
-                      WebMessageReceivedInfoType? infoType =
-                          info.data.stringToEnum(info.data.type);
-                      if (infoType == WebMessageReceivedInfoType.qrScan) {
-                        // 二维码扫描
-                        final result = await Get.toNamed(AppRoutes.qrCode,
-                            arguments:
-                                QrCodeScreenArgs(type: QrCodeType.webScan));
-                        controller.webViewController?.evaluateJavascript(
-                            source: 'window["webViewHandler"]("$result")');
-                      } else if (infoType ==
-                          WebMessageReceivedInfoType.downLoadImg) {
-                        // 下载图片
-                        if (info.data.base64 != null) {
-                          bool permission = await requestPermission(context);
-                          if (permission) {
-                            if (info.data.base64!.contains("data:image")) {
-                              info.data.base64 =
-                                  info.data.base64!.split(",").last;
+
+    Future<bool> _showExitConfirmation(BuildContext context) async {
+      return await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('确认退出？'),
+          content: Text('您确定要退出当前页面吗？'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(false),
+              child: Text('取消'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(true),
+              child: Text('退出'),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return WillPopScope(
+      onWillPop: () async {
+        // 自定义逻辑：比如显示弹窗或回到上一页
+        if (controller.webViewController != null) {
+          if (await controller.webViewController!.canGoBack()) {
+            controller.webViewController!.goBack();
+            return false;
+          }
+        }
+        return true;
+      },
+      child: Scaffold(
+          backgroundColor: BaseColors.white,
+          body: SafeArea(
+            child: InAppWebView(
+                initialUrlRequest: URLRequest(
+                    url: WebUri("https://prod.d2y6t2vezku3nd.amplifyapp.com/")),
+                initialSettings: InAppWebViewSettings(
+                  allowsInlineMediaPlayback: true, // 允许内嵌视频播放
+                  mediaPlaybackRequiresUserGesture: false, // 自动播放视频不需要用户手势
+                ),
+                onWebViewCreated: (webController) {
+                  controller.webViewController = webController;
+                  // 注册 JavaScript Handler
+                  webController.addJavaScriptHandler(
+                    handlerName: 'onMessageReceived',
+                    callback: (args) async {
+                      if (args.isNotEmpty) {
+                        Map<String, dynamic> parsedMessage =
+                            jsonDecode(args.first);
+                        WebMessageReceivedInfo info =
+                            WebMessageReceivedInfo.fromJson(parsedMessage);
+                        WebMessageReceivedInfoType? infoType =
+                            info.data.stringToEnum(info.data.type);
+                        if (infoType == WebMessageReceivedInfoType.qrScan) {
+                          // 二维码扫描
+                          final result = await Get.toNamed(AppRoutes.qrCode,
+                              arguments:
+                                  QrCodeScreenArgs(type: QrCodeType.webScan));
+                          controller.webViewController?.evaluateJavascript(
+                              source: 'window["webViewHandler"]("$result")');
+                        } else if (infoType ==
+                            WebMessageReceivedInfoType.downLoadImg) {
+                          // 下载图片
+                          if (info.data.base64 != null) {
+                            bool permission = await requestPermission(context);
+                            if (permission) {
+                              if (info.data.base64!.contains("data:image")) {
+                                info.data.base64 =
+                                    info.data.base64!.split(",").last;
+                              }
+                              await saveImageFromBase64(info.data.base64!);
+                              ToastUtils.showToast(
+                                  title: tr('chat.image_saved_successfully'));
                             }
-                            await saveImageFromBase64(info.data.base64!);
-                            ToastUtils.showToast(
-                                title: tr('chat.image_saved_successfully'));
                           }
-                        }
-                      } else if (infoType ==
-                          WebMessageReceivedInfoType.openOutLink) {
-                        // 跳转到浏览器
-                        launchUrl(Uri.parse(info.data.url ?? ''));
-                      } else if (infoType ==
-                          WebMessageReceivedInfoType.toMessage) {
-                        if (info.data.chatUserId != null &&
-                            info.data.chatUserSig != null) {
-                          final CoreServicesImpl coreInstance =
-                              TIMUIKitCore.getInstance();
+                        } else if (infoType ==
+                            WebMessageReceivedInfoType.openOutLink) {
+                          // 跳转到浏览器
+                          launchUrl(Uri.parse(info.data.url ?? ''));
+                        } else if (infoType ==
+                            WebMessageReceivedInfoType.toMessage) {
+                          if (info.data.chatUserId != null &&
+                              info.data.chatUserSig != null) {
+                            final CoreServicesImpl coreInstance =
+                                TIMUIKitCore.getInstance();
 
-                          // 检查当前是否已有登录用户
-                          if (controller.loginImId.isNotEmpty) {
-                            // 当前用户与新用户不同，需要登出并重新登录
-                            if (controller.loginImId != info.data.chatUserId) {
-                              await coreInstance.logout();
-                              await TencentImSDKPlugin.v2TIMManager.logout();
-                              await TUICallKit.instance.logout();
+                            // 检查当前是否已有登录用户
+                            if (controller.loginImId.isNotEmpty) {
+                              // 当前用户与新用户不同，需要登出并重新登录
+                              if (controller.loginImId != info.data.chatUserId) {
+                                await coreInstance.logout();
+                                await TencentImSDKPlugin.v2TIMManager.logout();
+                                await TUICallKit.instance.logout();
 
+                                final result = await coreInstance.login(
+                                  userID: info.data.chatUserId!,
+                                  userSig: info.data.chatUserSig!,
+                                );
+
+                                if (result.code == 0) {
+                                  controller.loginImId = info.data.chatUserId!;
+                                  await TUICallKit.instance.login(
+                                    20002781,
+                                    info.data.chatUserId!,
+                                    info.data.chatUserSig!,
+                                  );
+                                  Get.toNamed(AppRoutes.home);
+                                } else {
+                                  ToastUtils.showToast(title: result.desc);
+                                }
+                              } else {
+                                // 当前用户与新用户相同，直接跳转到首页
+                                Get.toNamed(AppRoutes.home);
+                              }
+                            } else {
+                              // 没有已登录用户，直接登录
                               final result = await coreInstance.login(
                                 userID: info.data.chatUserId!,
                                 userSig: info.data.chatUserSig!,
@@ -112,74 +166,53 @@ class MainScreen extends GetView<MainScreenController> {
                               } else {
                                 ToastUtils.showToast(title: result.desc);
                               }
-                            } else {
-                              // 当前用户与新用户相同，直接跳转到首页
-                              Get.toNamed(AppRoutes.home);
                             }
                           } else {
-                            // 没有已登录用户，直接登录
-                            final result = await coreInstance.login(
-                              userID: info.data.chatUserId!,
-                              userSig: info.data.chatUserSig!,
-                            );
-
-                            if (result.code == 0) {
-                              controller.loginImId = info.data.chatUserId!;
-                              await TUICallKit.instance.login(
-                                20002781,
-                                info.data.chatUserId!,
-                                info.data.chatUserSig!,
-                              );
-                              Get.toNamed(AppRoutes.home);
-                            } else {
-                              ToastUtils.showToast(title: result.desc);
-                            }
+                            // chatUserId 或 chatUserSig 为空
+                            ToastUtils.showToast(
+                                title: 'chatUserId 或 chatUserSig为空');
                           }
-                        } else {
-                          // chatUserId 或 chatUserSig 为空
-                          ToastUtils.showToast(
-                              title: 'chatUserId 或 chatUserSig为空');
+                        } else if (infoType ==
+                            WebMessageReceivedInfoType.changeLanguage) {
+                          StorageUtils.write(
+                              StorageKeys.currentLocale, info.data.language);
+                          LanguageEnum languageEnum = LanguageEnum.en;
+                          Locale locale =
+                              const Locale.fromSubtags(languageCode: "en");
+                          if (info.data.language == 'zh-cn') {
+                            locale = const Locale.fromSubtags(
+                                languageCode: 'zh', scriptCode: "Hans");
+                            languageEnum = LanguageEnum.zhHans;
+                          }
+                          context.setLocale(locale);
+                          Get.updateLocale(locale);
+                          Future.delayed(const Duration(milliseconds: 1), () {
+                            I18nUtils(null, languageEnumToString[languageEnum]);
+                          });
+                          // RestartWidget.restartApp(context);
+                        } else if (infoType ==
+                            WebMessageReceivedInfoType.logout) {
+                          controller.loginImId = '';
+                          await TIMUIKitCore.getInstance().logout();
+                          await TencentImSDKPlugin.v2TIMManager.logout();
+                          await TUICallKit.instance.logout();
                         }
-                      } else if (infoType ==
-                          WebMessageReceivedInfoType.changeLanguage) {
-                        StorageUtils.write(
-                            StorageKeys.currentLocale, info.data.language);
-                        LanguageEnum languageEnum = LanguageEnum.en;
-                        Locale locale =
-                            const Locale.fromSubtags(languageCode: "en");
-                        if (info.data.language == 'zh-cn') {
-                          locale = const Locale.fromSubtags(
-                              languageCode: 'zh', scriptCode: "Hans");
-                          languageEnum = LanguageEnum.zhHans;
-                        }
-                        context.setLocale(locale);
-                        Get.updateLocale(locale);
-                        Future.delayed(const Duration(milliseconds: 1), () {
-                          I18nUtils(null, languageEnumToString[languageEnum]);
-                        });
-                        // RestartWidget.restartApp(context);
-                      } else if (infoType ==
-                          WebMessageReceivedInfoType.logout) {
-                        controller.loginImId = '';
-                        await TIMUIKitCore.getInstance().logout();
-                        await TencentImSDKPlugin.v2TIMManager.logout();
-                        await TUICallKit.instance.logout();
                       }
+                    },
+                  );
+                },
+                onLoadStop: (controller, url) {
+                  // 注入一个JS方法，监听postMessage的调用
+                  controller.evaluateJavascript(source: """
+                  window.scriptHandler = {
+                    postMessage: function(message) {
+                      window.flutter_inappwebview.callHandler('onMessageReceived', message);
                     }
-                  },
-                );
-              },
-              onLoadStop: (controller, url) {
-                // 注入一个JS方法，监听postMessage的调用
-                controller.evaluateJavascript(source: """
-                window.scriptHandler = {
-                  postMessage: function(message) {
-                    window.flutter_inappwebview.callHandler('onMessageReceived', message);
-                  }
-                };
-              """);
-              }),
-        ));
+                  };
+                """);
+                }),
+          )),
+    );
   }
 
   // 请求权限的函数
