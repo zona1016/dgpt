c90195c0c91d3b7761af7c1897d6c1ef2a16a24c
zona <>
2025-02-11 15:06:10 +0800

    修改配套详情url

--- lib/screens/hashrate/hashrate_rental_detail_screen.dart
+++ lib/screens/hashrate/hashrate_rental_detail_screen.dart
@@ -6,6 +6,7 @@ import 'package:dgpt/utils/theme/color.dart';
 import 'package:dgpt/utils/theme/typography.dart';
 import 'package:dgpt/widget/base/base_app_bar.dart';
 import 'package:dgpt/widget/base/base_button.dart';
+import 'package:dgpt/widget/base/base_network_image.dart';
 import 'package:dgpt/widget/base/base_screen.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
@@ -244,8 +245,9 @@ class HashrateRentalDetailScreen
               ),
               child: Row(
                 children: [
-                  Image.asset(
-                    'assets/images/home/income_detail_icon.png',
+                  BaseNetworkImage(
+                    imageURL: controller.planDetail.value?.graphicsIconFileIdUrl ?? '',
+                    placeholder: 'assets/images/home/income_detail_icon.png',
                     height: 46,
                     width: 100,
                   ),
